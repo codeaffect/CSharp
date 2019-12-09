f1a1534d402fd59a900db6788dd1a20aa9f2e35f
sandeepnellutla <sandeep.nellutla@gmail.com>
2019-12-08 20:51:43 -0600

    staircase

---
+++ algorithms/Staircase/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Staircase
+{
+    class Program
+    {
+        // Complete the staircase function below.
+        static void staircase(int n)
+        {
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(new string(' ', n - i) + new string('#', i));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            staircase(n);
+        }
+    }
+}
