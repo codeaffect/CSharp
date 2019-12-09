171543465ce48fd98e49ff7f4756a47a1494e692
sandeepnellutla <sandeep.nellutla@gmail.com>
2019-12-08 21:58:24 -0600

    dev

---
+++ algorithms/TimeConversion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        /*
+        * Complete the timeConversion function below.
+        */
+        static string timeConversion(string s)
+        {
+            /*
+            * Write your code here.
+            */
+
+        }
+
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            string result = timeConversion(s);
+
+            Console.WriteLine(result);
+        }
+    }
+}
