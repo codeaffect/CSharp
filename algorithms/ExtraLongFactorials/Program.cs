469ff7ee9a11296a6041a3e32bfc9bb06d0edf61
sandeepnellutla <sandeep.nellutla@gmail.com>
2019-12-26 21:15:08 -0600

    largefactorial

---
+++ algorithms/ExtraLongFactorials/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+namespace ExtraLongFactorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            var factorial = BigInteger.One;
+            for (int i = 1; i <= n; i++)
+                factorial = factorial * i;
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
