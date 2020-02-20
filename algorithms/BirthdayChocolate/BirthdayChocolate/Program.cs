9939e2eddac988b9a69e4daaec0a104aa0758eea
Nellutla <snellutla@pv.local>
2020-02-20 14:16:49 -0600

    new

---
+++ algorithms/BirthdayChocolate/BirthdayChocolate/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayChocolate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;// Convert.ToInt32(Console.ReadLine().Trim());
+
+            var data = "1 2 1 3 2";// Console.ReadLine().TrimEnd();
+            List<int> s = data.Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+            data = "3 2";// Console.ReadLine().TrimEnd();
+            string[] dm = data.Split(' ');
+
+            int d = Convert.ToInt32(dm[0]);
+
+            int m = Convert.ToInt32(dm[1]);
+
+            int result = Birthday(s, d, m);
+        }
+
+        static int Birthday(List<int> s, int d, int m)
+        {
+            int result = 0;
+            int runningCount = 0;
+            int runningSum = 0;
+            for (int i = 0; i < s.Count; i++)
+            {
+                runningSum += s[i];
+                runningCount++;
+                if (runningCount == m)
+                {
+                    if (runningSum == d)
+                    {
+                        result++;
+                    }
+
+                    i = i - runningCount + 1;
+                    runningCount = runningSum = 0;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ algorithms/Migratory Birds/Migratory Birds/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migratory_Birds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrCount = 11;// Convert.ToInt32(Console.ReadLine().Trim());
+            var data = "1 2 3 4 5 4 3 2 1 3 4";// Console.ReadLine().TrimEnd();
+            List<int> arr = data.Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int result = MigratoryBirds(arr);
+        }
+
+        static int MigratoryBirds(List<int> arr)
+        {
+            var list = arr
+                        .GroupBy(c => c)
+                        .Select(d => new
+                        {
+                            id = d.Key,
+                            count = d.Count()
+                        })
+                        .OrderBy(x => x.count)
+                        .ToList();
+
+            list.ForEach(i => Console.WriteLine($"{i.id} -- {i.count}"));
+
+            var maxValue = list.Any(c => c.count>1);
+            if (maxValue)
+            {
+                return list.Where(c => c.count>1).OrderByDescending(o => o.count).ThenBy(t => t.id).First().id;
+            }
+            return list.OrderBy(c=>c.id).First().id;
+        }
+    }
+}
